869c5eb282e04482313d500ce6466017841b2f31
dcalenzani <dcalenzani@pucp.pe>
2023-07-15 00:35:57 -0500

    Finished code for Scielo, grabs the data from as many pages as you want, added some tags too to the final table.

--- README.md
+++ README.md
@@ -6,17 +6,28 @@ The code is extensively commented, as I currently still feel jupiter notebooks a
 ## Log
 The log contains the information of this version, the previous ones and the planned implementations.
 
+#### V.1.2
+- Loop for accesing all the articles in all pages of Scielo
+- Functions for updating the url dynamic handling of the URL
+- Prints for the user the progress of the page handling (at console)
+- Variables for pages and approximate (+15) number of articles
+- Added DOI and Country tags to the output
+
 #### V.1.1
 - Crawler gathers Authors, Title, Abstract and Id from the first page of Scielo
 - Crawler changed dictionary and arrays for a dictionary with classes, which are somesort of the same thing actually? But easier to call in a Python environment I guess
 - Divided some functions into smaller functions so they could be worked with more flexibility 
 
+#### V.1.0
+- Crawler gets the titles and authors information from the first page of scielo
+
 #### Planned implementations (in order of importance)
-- Return to the user the number of pages and approximate number of articles
-- Loop for accesing the articles in the other pages of the search
+- Limiter for the loop function, so you wont gather more articles than needed
+- GUI for people who don't code at all.
+- Packaging for beta version
 - Graph creation through matplotlib and networkx libraries
 - Functions for searching in Redalyc
-- Return to the user the progress with the CSV
+- OpenTranslate Abstract correlation
 
 ## Getting the page structure
 For each page we have different HTML structures. In fact, HTML is such a dumpster fire that you can find many libraries that will help you with the random stuff you can find produced by a rage infused coder at 4am.
@@ -24,7 +35,9 @@ For each page we have different HTML structures. In fact, HTML is such a dumpste
 If you are unfamiliar with HTML I would recommend just using the developer tools and then the tool that says "pick an element" or something along the lines. In Firefox you can use CTRL+SHIFT+C or find the button on the upper left side of the developer tools window.
 After doing this you should see the HTML code for the webpage and your cursor acting like a highlighter depending on its position on the site. You can see the HTML code moving, you can right click to copy the inner and outer HTML, I would recommend copying the outer html from the parent `<div>` (usually div) where you wanted contents are contained, always try to find the bigger container (the grand grand grand parent, if you wish). 
 
-### Further notes for future implementation
+Scielo is a more regular webpage, and seems to be statically loaded after gathering the information from the db server. Redalyc on the other side seems to be more dinamically, so it uses only one URL for all the article pages.
+
+## Further notes for future implementation
 Please note the current analysis on the Scielo website.
 - Scielo uses `<a>` for its next page buttons, while also using "javascript:go_to_page('2')". This can be useful for making the loop for all the pages. But the URL also lets you calculate the length of pages.
 - Author are inside a `<div>` called  line authors, inside theres `<a>` with the authors names.
--- acaCrawler.py
+++ acaCrawler.py
@@ -1,10 +1,18 @@
 ''' AcaCrawler V.1.1
 Academic Crawler for Scielo and Redalyc. Both digital libraries focus on distributing online open access journal, playing one of the most important roles in promoting open access to scientific research, facilitating access to many researchers of the region
-.
-Aca Crawler allows the user to automate its research by transforming first search URL into a CSV table with all the results and presenting it into a relational network.
 
-As of now, AcaCrawler only outputs in the CSV the data from the first webpage. But further updates will include all searches.
+Aca Crawler allows the user to automate its research by transforming first search URL into a CSV table with the most relevant data from each article (Authors, DOI (if found), Country, Title, Abstract)
+
+I feel that Jupiter Notebooks are a little bit clunky, so let me know if any of the comments are difficult to understand or maybe more should be added.
+
+The organization of the code goes like this:
+1 - Libraries
+2 - Boilerplate and variables
+3 - Scraping functions
+4 - Scraping process
+5 - CSV appending
 ''' 
+
 # Libraries for the implementation
 from bs4 import BeautifulSoup # Parsing and handling HTML Tree
 import ssl # SSL HTTPS Certificates
@@ -12,14 +20,7 @@ import csv # CSV handling
 import urllib.request, urllib.parse, urllib.error #URL encoding and handling
 from urllib.parse import quote
 
-# User search input
-search_text = input('Ingrese su busqueda: ')
-parsed_search = urllib.parse.quote(search_text) # Encode the search string() into url format
-
-# Base urls - The search urls stripped of the search query (right now only Scielo is supported)
-scielo_base_url = 'https://search.scielo.org/?lang=es&count=15&from=0&output=site&sort=&format=summary&fb=&page=1&q='
-
-# Ignore SSL errors (Scielo and Redalyc are trustable, I guess, but be careful as server impersonation might be possible even tough unlikely)
+# Ignore SSL errors (Scielo and Redalyc are trustable, I guess, but be careful as server impersonation might be possible (even thought unlikely)
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
@@ -29,10 +30,11 @@ articles = {} # Dictionary for placing Article objects
 
 class Article:
     def __init__(self):
+        self.doi = None
+        self.country = None
         self.title = None
         self.authors = []
         self.abstracts = []
-
 # Functions
 
 # Make the soup - Handle the web page
@@ -40,6 +42,21 @@ def soup(url):
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
     return soup
+
+# Total number of pages
+def get_scielo_total_pages(soup):
+    input_tag = soup.find('input', class_='form-control goto_page')
+    total_n_pages = int(input_tag.contents[0].strip().split()[1])
+    return total_n_pages
+
+# Function for updating the base url given some sort of search text and values
+def update_url(search_text,page_number,starting_article):
+    
+    scielo_base_url = f'https://search.scielo.org/?lang=es&count=15&from={starting_article}&output=site&sort=&format=summary&fb=&page={page_number}&q='
+    # redalyc_base_url = 'https://www.redalyc.org/busquedaArticuloFiltros.oa?q=motivaciones'
+    scielo_search = scielo_base_url + search_text
+
+    return scielo_search
 
 # The big boy (or girl) - It works by asking for a URL, which can (and will be) the search input we asked for at the beggining
 def get_data(url):
@@ -62,6 +79,7 @@ def get_data(url):
         # Check if our Article already exists. If it doesn't we create it, if it does then we append the classes to the corresponding attribute.
         if div_id not in articles:
             articles[div_id] = Article()
+            articles[div_id].country = div_id[-3:]
         match attr:
             case 'title':
                 articles[div_id].title = name
@@ -69,6 +87,8 @@ def get_data(url):
                 articles[div_id].authors.append(name)
             case 'abstract':
                 articles[div_id].abstracts.append(name)
+            case 'DOIResults':
+                articles[div_id].doi = name
         return
 
     # Get the data for each attribute (is there a less repetitive way of doing this?)
@@ -83,17 +103,55 @@ def get_data(url):
     for tag in get_tag_and_attr('div', 'abstract'):
         get_parent_id(tag)
         class_filler(tag, 'abstract')
+
+    for tag in get_tag_and_attr('span', 'DOIResults'):
+        get_parent_id(tag)
+        class_filler(tag, 'DOIResults')
     
     return articles
 
-# Mix the search with the base url
-scielo_search = scielo_base_url+parsed_search
+# Scielo url navigational variables
+articles_per_page = 15
+starting_article = 1
+page_number = 1
 
-# Get all the articles related to the search
-all_articles = get_data(scielo_search)
+# Define the fieldnames for the .csv (same as classes, might be a more dynamic solution)
+fieldnames = ['Id', 'Title', 'Authors', 'Abstracts', 'DOI', 'Country']
 
-# Define the fieldnames for the .csv
-fieldnames = ['Id', 'Title', 'Authors', 'Abstracts']
+# User search input (parsed)
+search_text = urllib.parse.quote(input('Ingrese su busqueda: '))
+
+# Update the variable for the first page
+base_url = update_url(search_text, page_number, starting_article)
+
+# Search for the tag that contains the totals number text
+input_tag = soup(base_url).find('input', class_='form-control goto_page')
+
+# Get the total number of pages for our search
+total_n_pages = int(input_tag.contents[0].strip().split()[1])
+
+# Aproximate the article number (to lowest)
+aprox_articles = articles_per_page*total_n_pages
+
+# Iterator loop
+print('Encontramos un aprox. de : ', aprox_articles, ' articulos')
+print('Estan repartidos entre: ',total_n_pages, ' paginas')
+limiter = int(input('Ingrese el numero de páginas para tabular: '))
+
+while page_number <= limiter:
+    # Print progress
+    print(f"Procesando pagina {page_number} de {total_n_pages}: {base_url}")
+    # Get all the articles related to the search
+    all_articles = get_data(base_url)
+    # Update the url
+    starting_article = starting_article + articles_per_page
+    page_number = page_number+1
+    base_url = update_url(search_text, page_number, starting_article)
+
+
+total_articles = len(all_articles)
+print('Articulos tabulados: ', len(all_articles))
+articles_processed = 0
 
 # Create a .csv files named as the searched text
 with open(f'{search_text}.csv', 'w', newline='') as file:
@@ -104,10 +162,12 @@ with open(f'{search_text}.csv', 'w', newline='') as file:
                 'Id' : article_id,
                 'Title': article.title,
                 'Authors':', '.join(article.authors),
-                'Abstracts': ', '.join(article.abstracts)
+                'Abstracts': ', '.join(article.abstracts),
+                'DOI' : article.doi,
+                'Country': article.country
             }
         writer.writerow(row)
-    print("CSV file created successfully.")
 
+print("CSV file created successfully.")
 '''
 '''
